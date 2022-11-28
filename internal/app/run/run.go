f6593c3cf75c6fed47af8391c43a15262ece9947
root <nameerrror3301@gmail.com>
2022-11-29 01:40:45 +0200

    Отрефакторил импорты и немного изменил вызовы функций

--- internal/app/run/run.go
+++ internal/app/run/run.go
@@ -5,9 +5,7 @@ import (
 	"time"
 
 	middle "NotesService/internal/middleware"
-	routes "NotesService/internal/routes"
-	auth "NotesService/internal/routes/auth"
-	"NotesService/internal/routes/notes"
+	"NotesService/internal/routes"
 )
 
 func Run() error {
@@ -22,11 +20,11 @@ func Run() error {
 
 	// http.HandleFunc("/api") -> There will be Swagger documentation
 	http.HandleFunc("/api/v1", middle.UserMethodCheck(middle.UserRequestLog(routes.HomePage), http.MethodGet, http.MethodPost))
-	http.HandleFunc("/api/v1/signUp", middle.UserMethodCheck(middle.UserCheckContent(auth.SignUp), http.MethodPost))
-	http.HandleFunc("/api/v1/notes", middle.UserMethodCheck(middle.UserBasicAuth(middle.UserRequestLog(notes.FindAllNotesOrById)), http.MethodGet))
-	http.HandleFunc("/api/v1/notes/create", middle.UserMethodCheck(middle.UserBasicAuth(middle.UserRequestLog(notes.CreateNote)), http.MethodPost))
-	http.HandleFunc("/api/v1/notes/upload", middle.UserMethodCheck(middle.UserBasicAuth(middle.UserRequestLog(notes.UploadNote)), http.MethodPut))
-	http.HandleFunc("/api/v1/notes/delete", middle.UserMethodCheck(middle.UserBasicAuth(middle.UserRequestLog(notes.DeleteNote)), http.MethodDelete))
+	http.HandleFunc("/api/v1/signUp", middle.UserMethodCheck(middle.UserCheckContent(routes.SignUp), http.MethodPost))
+	http.HandleFunc("/api/v1/notes", middle.UserMethodCheck(middle.UserBasicAuth(middle.UserRequestLog(routes.FindAllNotesOrById)), http.MethodGet))
+	http.HandleFunc("/api/v1/notes/create", middle.UserMethodCheck(middle.UserBasicAuth(middle.UserRequestLog(routes.CreateNote)), http.MethodPost))
+	http.HandleFunc("/api/v1/notes/upload", middle.UserMethodCheck(middle.UserBasicAuth(middle.UserRequestLog(routes.UploadNote)), http.MethodPut))
+	http.HandleFunc("/api/v1/notes/delete", middle.UserMethodCheck(middle.UserBasicAuth(middle.UserRequestLog(routes.DeleteNote)), http.MethodDelete))
 
 	if err := serv.ListenAndServe(); err != nil {
 		return err
